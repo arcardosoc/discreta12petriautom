a0fde67c889ad5ffa87edbd8e9529b392d6be6ed
Mateus Lenier Rezende <mateuslenier@gmail.com>
2015-12-03 19:08:10 -0300

    Adicionado funces secundarias

--- ex12.c
+++ ex12.c
@@ -1,11 +1,29 @@
 /* Programa Rede de Petri com threads */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
 #include <allegro.h>
 
+void inserirlista((void *)petri);
+void desenhaauto((void *)petri);
+void pthreadpetri((void *)petri);
+
 int main(void)
+{
+    return EXIT_SUCCESS;
+}
+
+void inserirlista((void *)petri)
+{
+    ;
+}
+
+void desenhaauto((void *)petri)
+{
+    ;
+}
+
+void pthreadpetri((void *)petri)
 {
     ;
 }
