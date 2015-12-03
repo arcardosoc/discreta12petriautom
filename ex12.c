0415a299350fefc9bbe77129ced51a1c3f72ebe2
Arthur Carvalho De Albuquerque Cardoso <arthurcardoso2005@hotmail.com>
2015-12-03 20:00:06 -0300

    mudanca de parametros

--- ex12.c
+++ ex12.c
@@ -4,9 +4,9 @@
 #include <pthread.h>
 #include <allegro.h>
 
-void inserirlista((void *)petri);
-void desenhaauto((void *)petri);
-void pthreadpetri((void *)petri);
+void inserirlista(petri **pet);
+void desenhaauto(petri *pet);
+void pthreadpetri(void);
 typedef struct st_fim
 {
     int tf;
@@ -19,7 +19,7 @@ typedef struct st_transicao
     struct st_transicao *prox;
 }transicao;
 
-typedef struct st_automato
+typedef struct st_petri
 { 
     int k, alf, si;
     fim lfim;
@@ -28,23 +28,23 @@ typedef struct st_automato
 
 int main(void)
 {
-    petri petri;
-    inserirlista(&petri);
-    desenhaauto(petri);
+    petri pet;
+    inserirlista(&pet);
+    desenhaauto(pet);
     return EXIT_SUCCESS;
 }
 
-void inserirlista((void *)petri)
+void inserirlista(petri **pet)
 {
     ;
 }
 
-void desenhaauto((void *)petri)
+void desenhaauto(petri *pet)
 {
     ;
 }
 
-void pthreadpetri((void *)petri)
+void pthreadpetri(void)
 {
     ;
 }
