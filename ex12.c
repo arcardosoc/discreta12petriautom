57781003f1de0efaa2044392658edcadc28d8b00
Mateus Lenier Rezende <mateuslenier@gmail.com>
2015-12-09 12:16:53 -0300

    codigo com paralelismo com bugs

--- ex12.c
+++ ex12.c
@@ -3,13 +3,26 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <allegro.h>
+#include <time.h>
 
 #define FNAME "entrada-petri-1.txt"
 #define VAZIO 0
 
+#ifndef NMAX
+#define NMAX 5
+#endif
+
+#ifndef PAT
+#define PAT 50
+#endif
+
+#ifndef DEBUG
+#define DEBUG 5
+#endif
+
 typedef struct st_lugartransicao
 {
-    int li, tf, tkp;
+    int li, tf, tkp, key;
     struct st_lugartransicao *prox;
 }lugartransicao;
 
@@ -27,75 +40,206 @@ typedef struct st_lugartoken
 
 typedef struct st_petri
 {
-    int ql, qt, qk, al, at;
+    int ql, qt, qk, al, at,key;
     lugartoken lntk;
     lugartransicao lutra;
     transicaolugar tralu;
 }petri;
 
-void desenhaauto(petri p);
-void simupetri(void);
+void desenhaauto(void);
+void *simupetri(void *p);
 void inserirlutk(lugartoken **cabeca,int lu,int tk);
-void inserirlutra(lugartransicao **cabeca,int lu,int tk,int trans);
+void inserirlutra(lugartransicao **cabeca,int lu,int tk,int trans,int key);
 void inserirtralu(transicaolugar **cabeca,int trans,int tk,int lu);
+void ativacaotransicao(transicaolugar *cabeca,lugartoken *pt,int ti);
+int retiratoken(lugartoken **cabeca, int lu, int tk);
+void addtoken(lugartoken **cabeca, int lu, int tk);
+void adicionai(petri **cabeca,int i);
 
 int main(void)
 {
-    int i,k,lu,trans,tk;
+    int i,k=0,lu,trans,tk;
     FILE *fl= fopen(FNAME,"r+");
     lugartoken *pt = NULL;
     lugartransicao *plt = NULL;
     transicaolugar *ptl = NULL;
-    petri p;
-    p.lntk.prox = NULL;
-    p.lutra.prox = NULL;
-    p.tralu.prox = NULL;
-    fscanf(fl,"%d",&(p.ql));
-    fscanf(fl,"%d",&(p.qt));
-    fscanf(fl,"%d",&(p.qk));
-    fscanf(fl,"%d",&(p.al));
-    fscanf(fl,"%d",&(p.at));
-    for(i = 0;i < p.qk;i++)
+    petri *p = malloc(sizeof(petri));
+    pthread_t pthread[NMAX];
+    printf("%d\n",p->lutra.tkp);
+    srand(time(NULL));
+    fscanf(fl,"%d",&(p->ql));
+    fscanf(fl,"%d",&(p->qt));
+    fscanf(fl,"%d",&(p->qk));
+    fscanf(fl,"%d",&(p->al));
+    fscanf(fl,"%d",&(p->at));
+    printf("Quantidade de Lugares:%d\nQuantidade de Transicoes:%d\n",p->ql,p->qt);
+    if(DEBUG > 0)
+        printf("Quantidade de Lugares com Tokens:%d\n Quantidade de Arcos Lugares:%d\nQuantidade de Arcos Transicoes:%d\n",p->qk,p->al,p->at);
+    desenhaauto();
+    for(i = 0;i < p->qk;i++)
     {
         fscanf(fl,"%d %d",&lu,&tk);
-        for(trans = k; trans < p.ql ;trans++)
+        for(trans = k; trans < p->ql ;trans++)
         {
-            pt = &(p.lntk);
-            if(i == lu)
-                inserirlutk(&pt,i,tk);
-            inserirlutk(&pt,i,VAZIO);
+            pt = &(p->lntk);
+            if(trans == lu)
+            {
+                if(DEBUG > 0)
+                    printf("Lugar:%d/Tokens:%d\n",lu,tk);
+                inserirlutk(&pt,lu,tk);
+                break;
+            }
+            else
+            {
+                if(DEBUG > 0)
+                    printf("Lugar:%d/Tokens:0\n",trans);
+                inserirlutk(&pt,trans,VAZIO);
+            }
         }
         k = trans+1;
-       
+
     }
-    if(k < p.ql)
-        for(i = k;i < p.ql; i++)
+    if(k < p->ql)
+        for(i = k;i < p->ql; i++)
+        {
+            if(DEBUG > 0)
+                printf("Lugar:%d/Tokens:0\n",i);
+            pt = &(p->lntk);
             inserirlutk(&pt,i,VAZIO);
-    for(i=0;i<p.al;i++)
+        }
+    for(i=0;i<p->al;i++)
     {
         fscanf(fl,"%d %d %d",&lu,&tk,&trans);
-        plt = &(p.lutra);
-        inserirlutra(&plt,lu,tk,trans);
+        if(DEBUG > 0)
+            printf("Lugar:%d---Tokens Perdidos:%d--->Transicao:%d\n",lu,tk,trans);
+        plt = &(p->lutra);
+        inserirlutra(&plt,lu,tk,trans,i);
     }
-    for(i=0;i<p.at;i++)
+    for(i=0;i<p->at;i++)
     {
         fscanf(fl,"%d %d %d",&trans,&tk,&lu);
-        ptl = &(p.tralu);
+        if(DEBUG > 0)
+            printf("Transicao:%d---Tokens Ganhos:%d--->Lugar:%d\n",trans,tk,lu);
+        ptl = &(p->tralu);
         inserirtralu(&ptl,trans,tk,lu);
     }
-    desenhaauto(p);
+    printf("|============INICIO SIMULACAO============|\n");
+    for(i=0;i < p->al;i++)
+    {
+        adicionai(&p,i);
+        if(pthread_create(&pthread[i], NULL, simupetri, (void *)p))
+        {
+            printf("\nFalha ao criar thread!");
+            return -1;
+        }
+        if(DEBUG > 1)
+            printf("Pthread[%d]: Criado com Sucesso\n",i);
+        sleep(1);
+    }
+    for(i = 0;i < p->al;i++)
+    {
+        pthread_join(pthread[i],NULL);
+        if(DEBUG > 1)
+            printf("Pthread[%d]: Fechado com Sucesso\n",i);
+    }
+    printf("|============FIM DA SIMULACAO============|\n");
+    fclose(fl);
     return EXIT_SUCCESS;
 }
 
+void adicionai(petri **cabeca,int i)
+{
+    petri *pl=*cabeca;
+    pl->key=i;
+    return;
+}
 
-void desenhaauto(petri p)
+void desenhaauto(void)
 {
     ;
 }
 
-void simupetri(void)
+void *simupetri(void *p)
 {
-    ;
+    int k,flag;
+    petri *ptemp = (petri *)p;
+    lugartoken *pt = NULL;
+    lugartransicao *plt = NULL;
+    transicaolugar *ptl = NULL;
+    plt=&(ptemp->lutra);
+    ptl=&(ptemp->tralu);
+    pt=&(ptemp->lntk);
+    while(plt->key != ptemp->key)
+        plt=plt->prox;
+    if(DEBUG > 2)
+        printf("Pthread[%d]/Key:%d\n",ptemp->key,plt->key);
+    for(k = 0;k < NMAX;k++)
+    {
+        if(DEBUG > 4)
+            printf("Pthread[%d]//Interacao[%d]:retirada de token\n",plt->key,k);
+        flag=retiratoken(&pt,plt->li,plt->tkp);
+        if(rand()%100+1 < PAT && flag)
+        {
+            if(DEBUG > 4)
+                printf("Pthread[%d]//Interacao[%d]:Transicao Ativada com Sucesso\n",plt->key,k);
+            ativacaotransicao(ptl,pt,plt->tf);
+        }
+    }
+    pthread_exit(0);
+}
+
+void ativacaotransicao(transicaolugar *cabeca,lugartoken *pt,int ti)
+{
+    transicaolugar *pl=cabeca;
+    while(pl!=NULL)
+    {
+        if(pl->ti == ti)
+        {
+            if(DEBUG > 4)
+                printf("Ativado Transicao:%d--->Lugar:%d\n",pl->ti,pl->lf);
+            addtoken(&pt,pl->lf,pl->tkg);
+        }
+        pl=pl->prox;
+    }
+    return;
+}
+
+int retiratoken(lugartoken **cabeca, int lu, int tk)
+{
+    lugartoken *pl=*cabeca;
+    while(pl!=NULL)
+    {
+        if(pl->lu == lu && pl->tk >= tk)
+        {
+            if(DEBUG > 2)
+                printf("Tko:%d - ",pl->tk);
+            pl->tk-=tk;
+            if(DEBUG > 2)
+                printf("Tk:%d = Tkf:%d\n",tk,pl->tk);
+            return 1;
+        }
+        pl=pl->prox;
+    }
+    return 0;
+}
+
+void addtoken(lugartoken **cabeca, int lu, int tk)
+{
+    lugartoken *pl=*cabeca;
+    while(pl!=NULL)
+    {
+        if(pl->lu == lu)
+        {
+            if(DEBUG > 2)
+                printf("Tko:%d + ",pl->tk);
+            pl->tk+=tk;
+            if(DEBUG > 2)
+                printf("Tk:%d = Tkf:%d\n",tk,pl->tk);
+            break;
+        }
+        pl=pl->prox;
+    }
+    return;
 }
 
 void inserirlutk(lugartoken **cabeca,int lu,int tk)
@@ -110,6 +254,8 @@ void inserirlutk(lugartoken **cabeca,int lu,int tk)
     pl = malloc(sizeof(lugartoken));
     pl->lu = lu;
     pl->tk = tk;
+    if(DEBUG > 2)
+        printf("Pl->lu:%d\nPl->tk:%d\n",pl->lu,pl->tk);
     pl->prox = NULL;
     if(plant != NULL)
         plant->prox = pl;
@@ -119,7 +265,7 @@ void inserirlutk(lugartoken **cabeca,int lu,int tk)
     return;
 }
 
-void inserirlutra(lugartransicao **cabeca,int lu,int tk,int trans)
+void inserirlutra(lugartransicao **cabeca,int lu,int tk,int trans,int key)
 {
     lugartransicao *pl = *cabeca;
     lugartransicao *plant = NULL;
@@ -132,6 +278,9 @@ void inserirlutra(lugartransicao **cabeca,int lu,int tk,int trans)
     pl->li = lu ;
     pl->tkp = tk;
     pl->tf = trans;
+    pl->key = key;
+    if(DEBUG > 2)
+        printf("Pl->li:%d\nPl->tkp:%d\nPl->tf:%d\nPl->key:%d\n",pl->li,pl->tkp,pl->tf,pl->key);
     pl->prox = NULL;
     if(plant != NULL)
         plant->prox = pl;
@@ -154,6 +303,8 @@ void inserirtralu(transicaolugar **cabeca,int trans,int tk,int lu)
     pl->ti = trans;
     pl->tkg = tk;
     pl->lf = lu;
+    if(DEBUG > 2)
+        printf("Pl->ti:%d\nPl->tkg:%d\nPl->lf:%d\n",pl->ti,pl->tkg,pl->lf);
     pl->prox = NULL;
     if(plant != NULL)
         plant->prox = pl;
